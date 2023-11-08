2ab276c9ee2d1404a1350ba4772324342cb239d0
Krit <>
2023-11-07 22:33:40 -0500

    first commit

---
+++ scripts/seed.ts
@@ -0,0 +1,21 @@
+const { PrismaClient } = require('@prisma/client');
+
+const db = new PrismaClient();
+
+async function main() {
+  try {
+    await db.category.createMany({
+      data: [
+        { name: "Entrepreneurs "},
+        { name: "Investors"},
+        { name: "Podcasters"}
+      ],
+    });
+  } catch (error) {
+    console.error('Error seeding default categories:', error);
+  } finally {
+    await db.$disconnect();
+  }
+}
+
+main();
